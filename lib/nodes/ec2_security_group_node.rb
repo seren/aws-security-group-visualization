df0ada68c7c1777d0c99cd6829d1c39cccf7c012
Seren <git@seren.net>
2018-02-27 00:53:40 -0800

    simplify aws node type determination

--- lib/nodes/ec2_security_group_node.rb
+++ lib/nodes/ec2_security_group_node.rb
@@ -1,10 +1,11 @@
 class Ec2SecurityGroupNode < Node
   attr_accessor :subgraph_id, :subgraphref, :owner_id, :vpc_id
 
-  def initialize(uid, name)
-    # When we set the name, set the type and the subgraph_id
-    ip_with_subnet = %r{\A(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)(?:\.(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)){3}(/\d\d?)?\z}
-    @type = name.match(ip_with_subnet) ? :cidr : :group
+  def initialize(uid, name, type)
+    # # When we set the name, set the type and the subgraph_id
+    # ip_with_subnet = %r{\A(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)(?:\.(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)){3}(/\d\d?)?\z}
+    # @type = name.match(ip_with_subnet) ? :cidr : :group
+    @type = type
     # puts "CREATED " + uid + " with type " + @type.to_s
     super
   end
